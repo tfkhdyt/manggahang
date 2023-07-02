7452d6f59f881b827bfaa3bbf0a12b93075381fb
tfkhdyt <tfkhdyt@proton.me>
2023-07-02 10:22:43 +0700

    feat: change font to Poppins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script>
 	import '@unocss/reset/tailwind.css';
 	import 'virtual:uno.css';
+	import '@fontsource/poppins/100.css';
+	import '@fontsource/poppins/200.css';
+	import '@fontsource/poppins/300.css';
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/500.css';
+	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/poppins/800.css';
+	import '@fontsource/poppins/900.css';
 	import '../app.css';
 
 	import Header from '../components/Header.svelte';
 	import Hero from '../components/Hero.svelte';
+	import TentangKami from '../components/TentangKami.svelte';
 </script>
 
 <svelte:head>
@@ -14,4 +24,5 @@
 <main class="container mx-auto lg:px-32">
 	<Header />
 	<Hero />
+	<TentangKami />
 </main>
