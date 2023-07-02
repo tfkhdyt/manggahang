60c699115e5744c6d54c052d86dc7adcac2d518d
tfkhdyt <tfkhdyt@proton.me>
2023-07-02 10:23:55 +0700

    feat: add tentang kami section

---
+++ src/components/TentangKami.svelte
@@ -0,0 +1,20 @@
+<section class="py-32 space-y-14" id="tentang-kami">
+	<div class="flex justify-between items-center space-x-4">
+		<h2 class="w-3/6 text-6xl font-bold leading-snug">Kenali Manggahang dalam 3 menit</h2>
+		<p class="w-2/6 text-xl leading-relaxed text-gray-600">
+			Manggahang adalah sebuah kelurahan di kecamatan Baleendah, Bandung, Jawa Barat, Indonesia.
+		</p>
+	</div>
+	<div>
+		<iframe
+			width="100%"
+			height="500"
+			src="https://www.youtube.com/embed/Yoa0rUk-O9Q"
+			title="Panorama KAMPUNG MANGGAHANG Hyperlapse"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+			class="rounded-[5rem]"
+		/>
+	</div>
+</section>
