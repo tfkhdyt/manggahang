4ec9dee4d173abc524d11aa611337017a19af658
tfkhdyt <tfkhdyt@proton.me>
2023-07-02 10:23:06 +0700

    feat(header): change button on navbar into anchor

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,12 @@
 <header class="flex justify-between items-center py-4">
 	<div class="flex items-center space-x-4">
 		<div class="p-3 font-bold text-white bg-blue-500 rounded-full w-fit">MG</div>
-		<p class="font-bold">Manggahang</p>
+		<p class="font-semibold">Manggahang</p>
 	</div>
 	<div class="space-x-12">
-		<button>Home</button>
-		<button>Tentang Kami</button>
-		<button>Visi Misi</button>
-		<button>Galeri</button>
+		<a href="#home">Home</a>
+		<a href="#tentang-kami">Tentang Kami</a>
+		<a href="#visi-misi">Visi Misi</a>
+		<a href="#galeri">Galeri</a>
 	</div>
 </header>
