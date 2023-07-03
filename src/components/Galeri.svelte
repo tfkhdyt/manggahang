498d327d88f99c498eaed51ee7b7f8423819d53e
tfkhdyt <tfkhdyt@proton.me>
2023-07-03 09:47:18 +0700

    feat(galeri): add more images

--- src/components/Galeri.svelte
+++ src/components/Galeri.svelte
@@ -1,4 +1,4 @@
-<section class="mt-32" id="galeri">
+<section class="py-16" id="galeri">
 	<h2 class="text-6xl font-bold leading-snug">Galeri</h2>
 	<div class="grid grid-cols-3 gap-10 mt-12">
 		<div class="relative group" title="SPBU Jelekong">
@@ -29,6 +29,36 @@
 				src="/img/grand-al-ihsan.jpeg"
 				alt="Grand Al Ihsan Residence"
 				class="object-cover object-center mx-auto w-full rounded transition rounded-tr-[4rem] aspect-square grayscale group-hover:grayscale-0"
+			/>
+		</div>
+		<div class="relative group" title="SD & SMP IT Fitrah Insani 2">
+			<div
+				class="absolute top-0 left-0 z-50 w-full bg-gradient-to-r from-orange-800 to-purple-800 rounded opacity-25 transition group-hover:opacity-0 rounded-bl-[4rem] aspect-square"
+			/>
+			<img
+				src="/img/fitrah-insani-2.jpeg"
+				alt="SD & SMP IT Fitrah Insani 2"
+				class="object-cover object-center mx-auto w-full rounded transition rounded-bl-[4rem] aspect-square grayscale group-hover:grayscale-0"
+			/>
+		</div>
+		<div class="relative group" title="Perumahan Bumi Siliwangi">
+			<div
+				class="absolute top-0 left-0 z-50 w-full bg-gradient-to-r from-orange-800 to-purple-800 rounded opacity-25 transition group-hover:opacity-0 aspect-square"
+			/>
+			<img
+				src="/img/perumahan-bumi-siliwangi.jpg"
+				alt="Perumahan Bumi Siliwangi"
+				class="object-cover object-center mx-auto w-full rounded transition aspect-square grayscale group-hover:grayscale-0"
+			/>
+		</div>
+		<div class="relative group" title="Modif Resto">
+			<div
+				class="absolute top-0 left-0 z-50 w-full bg-gradient-to-r from-orange-800 to-purple-800 rounded opacity-25 transition group-hover:opacity-0 rounded-br-[4rem] aspect-square"
+			/>
+			<img
+				src="/img/modif-resto.jpg"
+				alt="Modif Resto"
+				class="object-cover object-center mx-auto w-full rounded transition rounded-br-[4rem] aspect-square grayscale group-hover:grayscale-0"
 			/>
 		</div>
 	</div>
