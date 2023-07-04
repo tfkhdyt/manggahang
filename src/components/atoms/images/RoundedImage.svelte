28a874ea7ce3bfa4842442a2ce86df4b516ed3b7
tfkhdyt <tfkhdyt@proton.me>
2023-07-04 17:16:15 +0700

    fix(gradient-image): fix wrong rounded breakpoint

--- src/components/atoms/images/RoundedImage.svelte
+++ src/components/atoms/images/RoundedImage.svelte
@@ -18,10 +18,10 @@
 			.with('l', () => 'rounded-l-[2rem] md:rounded-l-[4rem]')
 			.with('r', () => 'rounded-r-[2rem] md:rounded-r-[4rem]')
 			.with('b', () => 'rounded-b-[2rem] md:rounded-b-[4rem]')
-			.with('tl', () => 'rounded-tl-[2rem] rounded-tl-[4rem]')
-			.with('tr', () => 'rounded-tr-[2rem] rounded-tr-[4rem]')
-			.with('bl', () => 'rounded-bl-[2rem] rounded-bl-[4rem]')
-			.with('br', () => 'rounded-br-[2rem] rounded-br-[4rem]')
+			.with('tl', () => 'rounded-tl-[2rem] md:rounded-tl-[4rem]')
+			.with('tr', () => 'rounded-tr-[2rem] md:rounded-tr-[4rem]')
+			.with('bl', () => 'rounded-bl-[2rem] md:rounded-bl-[4rem]')
+			.with('br', () => 'rounded-br-[2rem] md:rounded-br-[4rem]')
 			.with('none', () => '')
 			.exhaustive(),
 		className
