0a0c6c56794c11895556b7ae12f8783d1ea5f514
tfkhdyt <tfkhdyt@proton.me>
2023-07-05 06:59:34 +0700

    fix(stat): adjust padding

--- src/components/molecules/Stat.svelte
+++ src/components/molecules/Stat.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class="p-7 space-y-3 transition md:p-10 lg:space-y-6 rounded-[2rem] bg-neutral-100 hover:bg-neutral-200"
+	class="p-7 space-y-3 transition md:p-8 lg:p-10 lg:space-y-6 rounded-[2rem] bg-neutral-100 hover:bg-neutral-200"
 >
 	<MediumText>{value}</MediumText>
 	<NormalText>{title}</NormalText>
