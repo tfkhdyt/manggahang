a8be42f26751b7d842793bdc2bb7adc03293138d
tfkhdyt <tfkhdyt@proton.me>
2023-07-04 08:16:11 +0700

    refactor(hero): remove unneeded molecules

--- src/components/molecules/Introduction/Introduction.svelte
+++ src/components/molecules/Introduction/Introduction.svelte
@@ -1,20 +1,13 @@
 <script>
-	import Stat from '../Stat/Stat.svelte';
 	import Subtitle from '../../atoms/typography/Subtitle.svelte';
 	import Title from '../../atoms/typography/Title.svelte';
 </script>
 
-<div class="space-y-10 w-1/2">
-	<div class="max-w-md">
-		<Title>Selamat Datang di Manggahang!</Title>
-	</div>
-	<div class="max-w-md">
-		<Subtitle>
-			Manggahang adalah sebuah kelurahan di kecamatan Baleendah, Bandung, Jawa Barat, Indonesia.
-		</Subtitle>
-	</div>
-	<div class="flex pt-12 space-x-8">
-		<Stat title="Total penduduk" value="38 rb" />
-		<Stat title="Suhu Rata-rata" value="27 °C" />
-	</div>
+<div class="max-w-md">
+	<Title>Selamat Datang di Manggahang!</Title>
+</div>
+<div class="max-w-md">
+	<Subtitle>
+		Manggahang adalah sebuah kelurahan di kecamatan Baleendah, Bandung, Jawa Barat, Indonesia.
+	</Subtitle>
 </div>
--- src/components/organisms/Hero/Hero.svelte
+++ src/components/organisms/Hero/Hero.svelte
@@ -1,9 +1,30 @@
 <script>
-	import HeroImages from '../../molecules/HeroImages/HeroImages.svelte';
+	import GradientImage from '../../molecules/GradientImage/GradientImage.svelte';
 	import Introduction from '../../molecules/Introduction/Introduction.svelte';
+	import Stat from '../../molecules/Stat/Stat.svelte';
 </script>
 
 <section class="flex items-end pt-6 pb-16" id="home">
-	<Introduction />
-	<HeroImages />
+	<div class="space-y-10 w-1/2">
+		<Introduction />
+		<div class="flex pt-12 space-x-8">
+			<Stat title="Total penduduk" value="38 rb" />
+			<Stat title="Suhu Rata-rata" value="27 °C" />
+		</div>
+	</div>
+	<div class="pl-8 space-y-4 w-1/2">
+		<GradientImage
+			src="/img/gunung-geulis.jpg"
+			alt="Gunung geulis"
+			roundedPosition="t"
+			className="aspect-[3/1]"
+		/>
+		<GradientImage src="/img/lapang-carik.jpg" alt="Lapangan Carik" className="aspect-[2.5/1]" />
+		<GradientImage
+			src="/img/kantor-kelurahan.jpg"
+			alt="Kantor kelurahan"
+			roundedPosition="b"
+			className="aspect-[4/3]"
+		/>
+	</div>
 </section>
