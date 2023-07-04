07059c86890b6d17007b152b4fdbad7861efc031
tfkhdyt <tfkhdyt@proton.me>
2023-07-04 17:17:11 +0700

    fix(image-grid): adjust rounded position according to breakpoint

--- src/components/molecules/ImageGrid.svelte
+++ src/components/molecules/ImageGrid.svelte
@@ -1,8 +1,13 @@
 <script>
+	import { onDestroy } from 'svelte';
 	import GradientImage from './GradientImage.svelte';
+	import { createMediaStore } from 'svelte-media-queries';
+
+	const isMobile = createMediaStore('(max-width: 768px)');
+	onDestroy(() => isMobile.destroy());
 </script>
 
-<div class="grid grid-cols-3 gap-6 mt-12 lg:gap-10">
+<div class="grid grid-cols-2 gap-6 md:grid-cols-3 lg:gap-10">
 	<GradientImage
 		src="/img/spbu.jpg"
 		alt="SPBU Jelekong"
@@ -12,25 +17,25 @@
 	<GradientImage
 		src="/img/smkn-7-baleendah.jpg"
 		alt="SMKN 7 Baleendah"
-		roundedPosition="none"
+		roundedPosition={$isMobile ? 'tr' : 'none'}
 		className="aspect-square"
 	/>
 	<GradientImage
 		src="/img/grand-al-ihsan.jpeg"
 		alt="Grand Al Ihsan Residence"
-		roundedPosition="tr"
+		roundedPosition={$isMobile ? 'none' : 'tr'}
 		className="aspect-square"
 	/>
 	<GradientImage
 		src="/img/fitrah-insani-2.jpeg"
 		alt="SD & SMP IT Fitrah Insani 2"
-		roundedPosition="bl"
+		roundedPosition={$isMobile ? 'none' : 'bl'}
 		className="aspect-square"
 	/>
 	<GradientImage
 		src="/img/perumahan-bumi-siliwangi.jpg"
 		alt="Perumahan Bumi Siliwangi"
-		roundedPosition="none"
+		roundedPosition={$isMobile ? 'bl' : 'none'}
 		className="aspect-square"
 	/>
 	<GradientImage
