64f0afa6cf0586c15948d9c5693e9b4b912c03c1
tfkhdyt <tfkhdyt@proton.me>
2023-07-04 19:43:59 +0700

    perf(page): comment out unused font imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script>
 	import '@unocss/reset/tailwind.css';
 	import 'virtual:uno.css';
-	import '@fontsource/poppins/100.css';
-	import '@fontsource/poppins/200.css';
-	import '@fontsource/poppins/300.css';
+	// import '@fontsource/poppins/100.css';
+	// import '@fontsource/poppins/200.css';
+	// import '@fontsource/poppins/300.css';
 	import '@fontsource/poppins/400.css';
 	import '@fontsource/poppins/500.css';
 	import '@fontsource/poppins/600.css';
 	import '@fontsource/poppins/700.css';
-	import '@fontsource/poppins/800.css';
-	import '@fontsource/poppins/900.css';
+	// import '@fontsource/poppins/800.css';
+	// import '@fontsource/poppins/900.css';
 	import '../app.css';
 
 	import Homepage from '../components/templates/Homepage.svelte';
