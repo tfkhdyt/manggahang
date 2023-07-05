315f7560adb2b29569c0a8ea4d55bc71da7bc847
tfkhdyt <tfkhdyt@proton.me>
2023-07-05 07:51:09 +0700

    fix(lokasi): adjust scroll mt for mobile

--- src/components/organisms/Lokasi.svelte
+++ src/components/organisms/Lokasi.svelte
@@ -3,7 +3,10 @@
 	import Title from '../atoms/typography/Title.svelte';
 </script>
 
-<section class="pt-10 pb-20 space-y-8 md:py-20 md:space-y-12 scroll-mt-12" id="lokasi">
+<section
+	class="pt-10 pb-20 space-y-8 md:py-20 md:space-y-12 scroll-mt-20 md:scroll-mt-12"
+	id="lokasi"
+>
 	<Title>Lokasi</Title>
 	<Map />
 </section>
